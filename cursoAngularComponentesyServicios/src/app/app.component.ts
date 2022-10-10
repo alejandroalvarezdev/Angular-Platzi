404690c3c6c9fa72632cf2b91eae9a160da814d1
alejandroalvarezdev <alejandroalvarezdev@gmail.com>
2022-10-10 01:10:00 -0500

    Input

--- cursoAngularComponentesyServicios/src/app/app.component.ts
+++ cursoAngularComponentesyServicios/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cursoAngularComponentesyServicios';
+  imgParent ="https://i.natgeofe.com/k/6496b566-0510-4e92-84e8-7a0cf04aa505/red-fox-portrait_square.jpg";
 }
